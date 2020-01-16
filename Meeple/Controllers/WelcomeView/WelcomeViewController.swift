6dc7aee3b09b7d400ae935721600fa23b6ed5158
ninjin8811 <fumiya8811@gmail.com>
2020-01-16 15:39:49 +0900

    3倍サイズのアイコン画像を追加した→WelcomeViewのボタンの見た目を整えた

--- Meeple/Controllers/WelcomeView/WelcomeViewController.swift
+++ Meeple/Controllers/WelcomeView/WelcomeViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    
+    @IBOutlet weak var facebookButton: UIButton!
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //ログインボタンの見た目の変更
+        facebookButton.setTitle("Facebookではじめる", for: .normal)
+        facebookButton.setImage(#imageLiteral(resourceName: "fb-icon-75"), for: .normal)
+        facebookButton.titleEdgeInsets.left = CGFloat(10)
+        facebookButton.imageEdgeInsets.left = CGFloat(-40)
+        mailButton.setTitle("メールアドレスではじめる", for: .normal)
+        mailButton.setImage(#imageLiteral(resourceName: "mail-icon-60"), for: .normal)
+        mailButton.titleEdgeInsets.left = CGFloat(15)
+        mailButton.imageEdgeInsets.left = CGFloat(-15)
     }
     
 
