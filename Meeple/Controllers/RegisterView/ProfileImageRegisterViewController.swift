6dc65534fa8d3975cbbf739f4d7c4e879253e13f
ninjin8811 <fumiya8811@gmail.com>
2020-03-04 04:17:35 +0900

    ProfileImageRegisterViewControllerを作成した。 プロフィールイメージ登録画面のStoryboardとViewControllerのパーツを紐付けした。

---
+++ Meeple/Controllers/RegisterView/ProfileImageRegisterViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileImageRegisterViewController.swift
+//  Meeple
+//
+//  Created by 吉野史也 on 2020/03/04.
+//  Copyright © 2020 Meeple. All rights reserved.
+//
+
+import UIKit
+
+class ProfileImageRegisterViewController: UIViewController {
+
+    @IBOutlet weak var profileImage1: UIImageView!
+    @IBOutlet weak var profileImage2: UIImageView!
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var skipButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func uploadButtonPressed(_ sender: Any) {
+    }
+    @IBAction func skipButtonPressed(_ sender: Any) {
+    }
+}
