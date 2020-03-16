e4cc0ee7976227810bac236ff1c3c723e05c76e7
ninjin8811 <fumiya8811@gmail.com>
2020-03-17 03:56:55 +0900

    VerifyRegisterViewとEndRegisterViewのStoryboardとViewControllerを作成して、パーツを紐付けた。

---
+++ Meeple/Controllers/RegisterView/EndRegisterViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EndRegisterViewController.swift
+//  Meeple
+//
+//  Created by 吉野史也 on 2020/03/17.
+//  Copyright © 2020 Meeple. All rights reserved.
+//
+
+import UIKit
+
+class EndRegisterViewController: UIViewController {
+
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+    }
+    
+}
---
+++ Meeple/Controllers/RegisterView/VerifyRegisterViewController.swift
@@ -0,0 +1,31 @@
+//
+//  VerifyRegisterViewController.swift
+//  Meeple
+//
+//  Created by 吉野史也 on 2020/03/17.
+//  Copyright © 2020 Meeple. All rights reserved.
+//
+
+import UIKit
+
+class VerifyRegisterViewController: UIViewController {
+
+    @IBOutlet weak var verifyBlock1: UIView!
+    @IBOutlet weak var verifyBlock2: UIView!
+    @IBOutlet weak var verifyImage1: UIImageView!
+    @IBOutlet weak var verifyImage2: UIImageView!
+    @IBOutlet weak var highlightView1: UIView!
+    @IBOutlet weak var highlightView2: UIView!
+    @IBOutlet weak var uploadButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func uploadButtonPressed(_ sender: Any) {
+    }
+    @IBAction func skipButtonPressed(_ sender: Any) {
+    }
+}
