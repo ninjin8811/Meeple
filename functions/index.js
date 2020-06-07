0eeec64e6239aac567e147cba96aa27b5cb40ca5
ninjin8811 <fumiya8811@gmail.com>
2020-06-07 17:01:42 +0900

    CloudFunctionsの環境変数にAlgoliaのkeyを入れた

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+const ALGOLIA_ID = functions.config().algolia.app_id
+const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key
+const ALGOLIA_SEARCH_KEY = functions.config().algolia.search_key
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+
+exports.helloWorld = functions.https.onRequest((req, res) => {
+    res.send("Hello");
+});
